5b7a555b88c668ba365c17e03f0e4461925df6d7
LuizRoberto18 <luiz.roberto20@yahoo.com>
2026-02-26 02:57:19 -0300

    substituindo row-body por stream/consumers, recriando metodo POST para chamados e adicionando arquivo body para teste, finalizando tarefa

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-import { parse } from "path";
-import { send } from "process";
+import { parse } from "url";
+import { json } from "stream/consumers";
 
 let chamados = [];
 let currentId = 1;
@@ -9,7 +9,7 @@ const sendJSON = (res, statusCode, data) => {
     res.writeHead(statusCode, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data));
 };
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { pathname } = parse(req.url, true);
 
     if (req.method === "GET" && pathname === "/health") {
@@ -32,6 +32,39 @@ const server = http.createServer((req, res) => {
         }
         return sendJSON(res, 200, chamado);
     }
+
+if (req.method === "POST" && pathname === "/chamados") {
+    try {
+        const data = await json(req);
+
+        const { solicitante, descricao, prioridade } = data;
+
+        if (!solicitante || !descricao || !prioridade) {
+            return sendJSON(res, 422, {
+                erro: "Campos obrigatórios: solicitante, descricao, prioridade"
+            });
+        }
+
+        const novoChamado = {
+            id: currentId++,
+            solicitante,
+            descricao,
+            prioridade
+        };
+
+        chamados.push(novoChamado);
+
+        return sendJSON(res, 201, novoChamado);
+
+    } catch (error) {
+        return sendJSON(res, 400, { erro: "JSON inválido" });
+    }
+}
+    // ===============================
+    // Rota não encontrada
+    // ===============================
+    return sendJSON(res, 404, { erro: "Rota não encontrada" });
+
 });
 
 server.listen(3000, () => {
